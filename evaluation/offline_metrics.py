6c7dda1ab7a21eab6794d7df1b7b2722a552b5d9
magantoine <antoine.magron@epfl.ch>
2024-04-12 10:39:20 -0400

    match case to clean offline metrix

--- evaluation/offline_metrics.py
+++ evaluation/offline_metrics.py
@@ -23,15 +23,17 @@ def self_BLEU(sentences: List[str]) -> float:
 def embSim(sentences: List[str],
            format:str="matrix"
            ) -> Union[List[List[float]], List[float]]:
-    if(format not in ["flat", "matrix"]):
-        raise ValueError(f"Unsupported output format : {format}, must be in ['flat', 'matrix'].")
     pairwise = pairwise_cosine_similarity(emb_sim_model.get_emb(sentences)).detach().numpy()
-    if(format == "matrix") :
-        return pairwise
-    elif(format == "flat"):
-        t = ((np.ones(pairwise.shape) - np.tri(pairwise.shape[0], pairwise.shape[1])) * pairwise).flatten()
-        t = t[t != 0]
-        return t
+    match format:
+        case "matrix":
+            return pairwise
+        case "flat":
+            t = ((np.ones(pairwise.shape) - np.tri(pairwise.shape[0], pairwise.shape[1])) * pairwise).flatten()
+            t = t[t != 0]
+            return t
+        case _:
+            raise ValueError(f"Unsupported output format : {format}, must be in ['flat', 'matrix'].")
+
     
 
 @expose
