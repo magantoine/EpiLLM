d9d004a3d6f1ce66cc203a3e30b1591d9587f392
Antoine Magron <62933498+magantoine@users.noreply.github.com>
2024-04-12 11:40:03 -0400

    Puts real .env file

--- setup.sh
+++ setup.sh
@@ -13,12 +13,12 @@ echo "
 
 (CRCHUM - 2024)
 "
-echo "DIR_PATH=\"$(pwd)\"" >> .envt
+echo "DIR_PATH=\"$(pwd)\"" >> .env
 
 echo "Enter your OpenAI API Key :"
 
 read api
 
-echo "API_KEY=\"$api\"" >> .envt
+echo "API_KEY=\"$api\"" >> .env
 
 ./dependencies.sh
